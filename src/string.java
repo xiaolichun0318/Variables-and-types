7e253edbe1b3ef80579c80cab917c08be9d7d987
xiaolichun0318 <2798640861@qq.com>
2025-02-28 10:51:52 +0800

    string

--- src/string.java
+++ src/string.java
@@ -9,24 +9,21 @@ public class SliceOHeaven {
     public String sides;
     public String drinks;
 
-    
     private String orderID;
     private double orderTotal;
-   
+
     public static final String DEF_ORDER_ID = "DEF-SOH-099";
     public static final String DEF_PIZZA_INGREDIENTS = "Mozzarella Cheese";
     public static final double DEF_ORDER_TOTAL = 15.00;
 
-    
     public SliceOHeaven() {
         this.orderID = DEF_ORDER_ID;
         this.pizzaIngredients = DEF_PIZZA_INGREDIENTS;
         this.orderTotal = DEF_ORDER_TOTAL;
-    this.sides = "";
-    this.drinks = "";
+        this.sides = "";
+        this.drinks = "";
     }
 
-    
     public SliceOHeaven(String storeName, String storeAddress, String storeEmail, long storePhone, String storeMenu) {
         this.storeName = storeName;
         this.storeAddress = storeAddress;
@@ -37,7 +34,7 @@ public class SliceOHeaven {
         this.pizzaIngredients = DEF_PIZZA_INGREDIENTS;
         this.orderTotal = DEF_ORDER_TOTAL;
         this.sides = "";
-        this.drinks = "";  
+        this.drinks = "";
     }
 
     public SliceOHeaven(String storeName, String storeAddress, String storeEmail, long storePhone, String storeMenu,
@@ -48,7 +45,6 @@ public class SliceOHeaven {
         this.orderTotal = orderTotal;
     }
 
-    
     public String getOrderID() {
         return orderID;
     }
@@ -89,7 +85,6 @@ public class SliceOHeaven {
         }
     }
 
-    
     private void printReceipt() {
         System.out.println("********RECEIPT********");
         System.out.println("Store Name: " + storeName);
@@ -100,10 +95,50 @@ public class SliceOHeaven {
         System.out.println("Order Total: $" + getOrderTotal());
     }
 
+    public void processCardPayment(String cardNumber, String expiryDate, int cvv) {
+        int cardLength = cardNumber.length();
+        if (cardLength == 14) {
+            System.out.println("Card accepted");
+        } else {
+            System.out.println("Invalid card");
+        }
+
+        int firstCardDigit = Integer.parseInt(cardNumber.substring(0, 1));
+
+        String blacklistedNumber = "12345678901234";
+        if (cardNumber.equals(blacklistedNumber)) {
+            System.out.println("Card is blacklisted. Please use another card");
+        }
+
+        int lastFourDigits = Integer.parseInt(cardNumber.substring(cardNumber.length() - 4));
+
+        StringBuilder cardNumberToDisplayBuilder = new StringBuilder(cardNumber);
+        for (int i = 1; i < cardNumber.length() - 4; i++) {
+            cardNumberToDisplayBuilder.setCharAt(i, '*');
+        }
+        String cardNumberToDisplay = cardNumberToDisplayBuilder.toString();
+
+        System.out.println("First digit of card: " + firstCardDigit);
+        System.out.println("Last four digits of card: " + lastFourDigits);
+        System.out.println("Card number to display: " + cardNumberToDisplay);
+    }
+
+    public void specialOfTheDay(String pizzaOfTheDay, String sideOfTheDay, String specialPrice) {
+        StringBuilder specialDetails = new StringBuilder();
+        specialDetails.append("Special of the day:\n");
+        specialDetails.append("Pizza: ").append(pizzaOfTheDay).append("\n");
+        specialDetails.append("Side: ").append(sideOfTheDay).append("\n");
+        specialDetails.append("Price: ").append(specialPrice).append("\n");
+        System.out.println(specialDetails.toString());
+    }
+
     public static void main(String[] args) {
-        SliceOHeaven pizzeria = new SliceOHeaven("Slice-o-Heaven", "123 Pizza St", "info@sliceoheaven.com", 1234567890L,
+        SliceOHeaven pizzeria = new SliceOHeaven("Slice - o - Heaven", "123 Pizza St", "info@sliceoheaven.com", 1234567890L,
                 "Pizza, Sides");
         pizzeria.pizzaPrice = 20.0;
-        pizzeria.takeOrder("SOH-123", "Pepperoni, Mushrooms", "Garlic Bread", "Cola");
+        pizzeria.takeOrder("SOH - 123", "Pepperoni, Mushrooms", "Garlic Bread", "Cola");
+
+        pizzeria.processCardPayment("12345678901234", "12/25", 123);
+        pizzeria.specialOfTheDay("Margherita Pizza", "Fries", "$12.99");
     }
 }
